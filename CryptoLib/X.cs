8031d48f776d3da8539e70cce32b2cb9f1ff4028
parnedoand1999 <parnedoand@educacion.navarra.es>
2021-01-31 13:37:27 +0100

    Ultimo  metodo completo

--- CryptoLib/X.cs
+++ CryptoLib/X.cs
@@ -142,7 +142,18 @@ namespace CryptoLib
 
         public static string ShaHash(Object input)
         {
-            return null;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+
+                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes((String)input));
+                var sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
         }
 
         public static string RandomString(int length)
