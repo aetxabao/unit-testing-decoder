7b51c4e1203c544c1c07bc067c7a00ebe1899f0f
ionene <ione8ibanez@gmail.com>
2021-02-04 19:27:09 +0100

    ione bukatuta

--- CryptoLib/X.cs
+++ CryptoLib/X.cs
@@ -38,41 +38,137 @@ namespace CryptoLib
         }
         private static RSAParameters RsaParsFromXml(string data)
         {
-            return new RSAParameters();
+            XmlSerializer xml = new XmlSerializer(typeof(RSAParameters));
+            object result;
+            using (TextReader reader = new StringReader(data))
+            {
+                result = xml.Deserialize(reader);
+            }
+            return (RSAParameters)result;
         }
 
         public static string RsaEncrypt(string text, string pubParsXml)
         {
-            return null;
+            RSAParameters parameters = RsaParsFromXml(pubParsXml);
+            byte[] data = Encoding.Default.GetBytes(text);
+            using (RSACryptoServiceProvider tester = new RSACryptoServiceProvider())
+            {
+                tester.ImportParameters(parameters);
+                byte[] encrypted = tester.Encrypt(data, false);
+                string base64 = Convert.ToBase64String(encrypted, 0, encrypted.Length);
+                return base64;
+            }
         }
 
         public static string RsaDecrypt(string code, RSACryptoServiceProvider rsa)
         {
-            return null;
+            byte[] encrypted = System.Convert.FromBase64String(code);
+            byte[] decrypted = rsa.Decrypt(encrypted, false);
+            string text = Encoding.UTF8.GetString(decrypted);
+            return text;
         }
+
         public static string SignedData(string text, RSACryptoServiceProvider rsa)
         {
-            return null;
+            byte[] data = Encoding.Default.GetBytes(text);
+            byte[] xdata = rsa.SignData(data, new SHA1CryptoServiceProvider());
+            string base64 = Convert.ToBase64String(xdata, 0, xdata.Length);
+            return base64;
         }
+
         public static bool VerifyData(string text, string signedText, string pubParsXml)
         {
-            return false;
+            RSAParameters parameters = RsaParsFromXml(pubParsXml);
+            byte[] data = Encoding.Default.GetBytes(text);
+            byte[] signedData = Convert.FromBase64String(signedText);
+            RSACryptoServiceProvider tester = new RSACryptoServiceProvider();
+            tester.ImportParameters(parameters);
+            return tester.VerifyData(data, new SHA1CryptoServiceProvider(), signedData);
         }
 
 
         public static string AesEncrypt(string msg, string pwd, out string iv)
         {
-            iv = "";
-            return null;
+            byte[] encrypted;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                byte[] key = Encoding.UTF8.GetBytes(pwd);
+                Array.Resize(ref key, 32);
+                aesAlg.Key = key;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(msg);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                    iv = Convert.ToBase64String(aesAlg.IV, 0, aesAlg.IV.Length);
+                }
+            
+            return Convert.ToBase64String(encrypted, 0, encrypted.Length);
         }
-        public static string AesDecrypt(string enc, string pwd, string sal)
+        
+
+    }
+    public static string AesDecrypt(string enc, string pwd, string sal)
         {
-            return null;
+            //try
+            //{
+
+                byte[] encrypted = System.Convert.FromBase64String(enc);
+
+                byte[] key = Encoding.UTF8.GetBytes(pwd);
+                Array.Resize(ref key, 32);
+                byte[] iv = System.Convert.FromBase64String(sal);
+                Array.Resize(ref iv, 16);
+
+                string plaintext = null;
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = key;
+                    aesAlg.IV = iv;
+
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                plaintext = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                return plaintext;
+            //}
+            //catch (Exception)
+            /*{
+                System.Console.WriteLine("Error      : ALGÚN PARÁMETRO ES INCORRECTO!!!");
+            }*/
+  
         }
 
         public static string ShaHash(Object input)
         {
-            return null;
+            using (SHA256 sha256Hash = SHA256.Create()){
+                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes((String)input));
+                var sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+            
         }
 
         public static string RandomString(int length)
@@ -93,11 +189,10 @@ namespace CryptoLib
             return res.ToString();
         }
 
-    }
-
     public class Utf8StringWriter : StringWriter
     {
         public override Encoding Encoding => Encoding.UTF8;
 
     }
+}
 }
